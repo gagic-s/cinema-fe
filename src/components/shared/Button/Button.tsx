6c30d137d5a18b889c285ce6954f6d502bcf9f8a
Sandra Radmanovic <s.radmanovic@vegait.rs>
2025-02-13 17:08:17 +0100

    Added custom button

---
+++ src/components/shared/Button/Button.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+interface ButtonProps {
+  text: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  variant?: "primary" | "secondary";
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  type = "button",
+  variant = "primary",
+  disabled = false,
+}) => {
+  return (
+    <button
+      className={`${styles.button} ${styles[variant]}`}
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
